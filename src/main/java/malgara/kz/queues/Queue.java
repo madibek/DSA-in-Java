fc76743720faedcfd99a9f452cd8704275ae4c6f
madibek1992@mail.ru <ayupov.m.b@gmail.com>
2018-07-18 22:43:25 +0600

    standard queue implementation (FIFO)

---
+++ src/main/java/malgara/kz/queues/Queue.java
@@ -0,0 +1,82 @@
+package malgara.kz.queues;
+
+import malgara.kz.linkedlist.Node;
+
+public class Queue {
+
+    Node head;
+    Node tail;
+    int count;
+
+    public void enqueue(int value) {
+        addLast(value);
+    }
+
+    private void addLast(int value) {
+
+        if (head == null) {
+            head = new Node(value);
+            tail = head;
+        } else {
+            Node node = new Node(value);
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+    }
+
+    public int dequeue() {
+
+        return removeFirst();
+    }
+
+    private int removeFirst() {
+
+        if (head == null) {
+            throw new RuntimeException("Queue is empty");
+        }
+
+        int value = head.value;
+        if (head.next == null) {
+            head = null;
+        } else {
+
+            head = head.next;
+        }
+        count--;
+        return value;
+    }
+
+    public int peek() {
+
+        if (head == null) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return head.value;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void clear() {
+
+        head = null;
+        count = 0;
+    }
+
+    public void traverse() {
+
+        if (head == null) {
+            return;
+        }
+
+        System.out.println("Queue items: ");
+        Node node = head;
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+}
---
+++ src/main/java/malgara/kz/queues/QueuesDemo.java
@@ -0,0 +1,32 @@
+package malgara.kz.queues;
+
+public class QueuesDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(QueuesDemo.class.getName());
+
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(40);
+        queue.enqueue(20);
+        queue.enqueue(90);
+        System.out.println("deque: " + queue.dequeue());
+        queue.traverse();
+        queue.enqueue(70);
+        queue.enqueue(80);
+        System.out.println("peek : " + queue.peek());
+        System.out.println("deque: " + queue.dequeue());
+        System.out.println("peek : " + queue.peek());
+        queue.traverse();
+        System.out.println("Queue size: " + queue.size());
+        queue.clear();
+        System.out.println("Queue size: " + queue.size());
+        try {
+            queue.dequeue();
+        } catch (RuntimeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
