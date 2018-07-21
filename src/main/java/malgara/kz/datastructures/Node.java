d334bb36702a5d2361fb2fdc2a158c2a3307309c
madibek1992@mail.ru <ayupov.m.b@gmail.com>
2018-07-21 15:24:17 +0600

    --

--- src/main/java/malgara/kz/datastructures/Node.java
+++ src/main/java/malgara/kz/datastructures/Node.java
@@ -2,11 +2,11 @@ package malgara.kz.datastructures;
 
 public class Node {
 
-    Node left;
-    Node right;
-    int value;
+    public Node left;
+    public Node right;
+    public int value;
 
-    Node(int value) {
+    public Node(int value) {
         this.value = value;
     }
 
