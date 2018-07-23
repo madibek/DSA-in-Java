20bc389b0697391d45eaaa388b32faa4ca8b6229
madibek1992@mail.ru <ayupov.m.b@gmail.com>
2018-07-23 22:29:27 +0600

    shell sort 2

--- src/main/java/malgara/kz/sorting/ShellSortDemo.java
+++ src/main/java/malgara/kz/sorting/ShellSortDemo.java
@@ -11,7 +11,7 @@ public class ShellSortDemo {
         int[] arrayToSort = {14, 18, 19, 37, 23, 40, 29, 30, 11, -1, 45, 67, 3, 5, 89, 5555, 44, 6, 2121};
         System.out.println("Array before sort: " + Arrays.toString(arrayToSort));
 
-        int[] sortedArray = shellSort(arrayToSort);
+        int[] sortedArray = shellSort2(arrayToSort);
         System.out.println("Array after sort: " + Arrays.toString(sortedArray));
     }
 
@@ -50,4 +50,40 @@ public class ShellSortDemo {
 
         return arrayToSort;
     }
-}
+
+    private static int[] shellSort2(int[] arr) {
+
+        if (arr == null) {
+            throw new RuntimeException("Array can not be null");
+        }
+
+        if (arr.length < 2) {
+            // already sorted array with one item
+            return arr;
+        }
+
+        int gap = arr.length / 2;
+        while (gap > 0) {
+
+            int start = gap;
+            while (start < arr.length) {
+
+                int value = arr[start];
+                int index = start - gap;
+                while (index >= 0 && value < arr[index]) {
+                    arr[index + gap] = arr[index];
+                    index = index - gap;
+                }
+
+                arr[index + gap] = value;
+                start++;
+            }
+
+            gap = gap / 2;
+        }
+
+        return arr;
+
+    }
+
+    }
